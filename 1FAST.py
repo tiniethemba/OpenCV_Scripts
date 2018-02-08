22f6f6e7f1ca19f9136452964613230d435e0ed7
tiniethemba <t.m.t.kaonga-14@student.lboro.ac.uk>
2018-02-08 22:42:04 +0000

    Changes made 08_02

--- 1FAST.py
+++ 1FAST.py
@@ -4,44 +4,43 @@ import cv2
 cap = cv2.VideoCapture(0)
 #cap1 = cv2.VideoCapture(1)
 r,f = cap.read()
-#r1,f1 = cap1.read()
-#fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter("output.avi", fourcc, 20.0, (640,480))
-try:
-    while(cap.isOpened()):
-        ret, frame = cap.read(cv2.IMREAD_COLOR)
-        #ret1,frame1 = cap1.read(cv2.IMREAD_COLOR)
-        # Initiate FAST object with default values
-        fast = cv2.FastFeatureDetector_create(threshold=7)
-        #fast1 = cv2.FastFeatureDetector_create(threshold=15)
-        # find and draw the keypoints
-        #cv2.FastFeatureDetector_create()
-        kp = fast.detect(frame,None)
-        #kp1 = fast1.detect(frame1, None)
 
-        #frame = cv2.drawKeypoints(frame, kp, color=(0,255,255),outImage=0,flags = 0)
-        #frame1 =  cv2.drawKeypoints(frame1, kp1, color=(0,255,255),outImage=0,flags = 0)
-        # Print all default params
-        print "Threshold: ", fast.getThreshold()
-        print "nonmaxSuppression: ", fast.getNonmaxSuppression()
-        print "neighborhood: ", fast.getType()
-        print "Total Keypoints with nonmaxSuppression: ", len(kp)
-        frame = cv2.resize(frame, (640,480))
-        cv2.rectangle(frame, (0,0), (630,270), (0,0,255), 2)
-        cv2.imshow('Computer WebCam',frame)
-        #cv2.imshow("External Webcam", frame1)
-        #out.write(frame1)
-        # Create black (zeroed pixels) image, the same size as the frame
-        mask = np.zeros((frame.shape[0],frame.shape[1], 3), np.uint8)
-        mask[:] = (0,0,0)
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Initiate FAST object with default values
+    fast = cv2.FastFeatureDetector_create(threshold=7)
+    kp = fast.detect(frame,None)
+    #kp1 = fast1.detect(frame1, None)
 
-        # Draw the kp's in yellow over the black image
-        fmask = cv2.drawKeypoints(mask, kp, None, color=(0, 255, 255), flags=0)
+    #frame = cv2.drawKeypoints(frame, kp, color=(0,255,255),outImage=0,flags = 0)
+    #frame1 =  cv2.drawKeypoints(frame1, kp1, color=(0,255,255),outImage=0,flags = 0)
+    # Print all default params
+    print "Threshold: ", fast.getThreshold()
+    print "nonmaxSuppression: ", fast.getNonmaxSuppression()
+    print "neighborhood: ", fast.getType()
+    print "Total Keypoints with nonmaxSuppression: ", len(kp)
+    frame = cv2.resize(frame, (640,480))
+    cv2.imshow('Computer WebCam',frame)
+    # Create black (zeroed pixels) image, the same size as the frame
+    mask = np.zeros((frame.shape[0],frame.shape[1], 3), np.uint8)
+    mask[:] = (0,0,0)
 
-        cv2.imshow('Keypoints only', fmask)
-        k = cv2.waitKey(10) & 0xFF
-        if k == 27:
-            break
+    # Draw the kp's in yellow over the black image
+    fmask = cv2.drawKeypoints(mask, kp, None, color=(255, 0, 0), flags=0)
+    frame1 = cv2.drawKeypoints(frame,kp,None,color= (0,0,0), flags=0)
+    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    __,cons ,_= cv2.findContours(frame1.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    print "CONS",len(cons)
+    for i in range(len(cons)):
+        x, y, w, h = cv2.boundingRect(cons[i])
+        cv2.rectangle(frame1,(x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.imshow('Keypoints only', fmask)
+    cv2.imshow("BLOW", frame1)
 
-except KeyboardInterrupt:
-    raise SystemExit
+    k = cv2.waitKey(10) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ ColourTrack.py
@@ -0,0 +1,169 @@
+import cv2
+import numpy as np
+
+class ColourTrack():
+    def __init__(self,cam, px_cols, px_rows, redBounds, greenBounds, blueBounds):
+        self.r_lowerBound = redBounds[0]
+        self.r_upperBound = redBounds[1]
+        self.g_lowerBound = greenBounds[0]
+        self.g_upperBound = greenBounds[1]
+        self.b_lowerBound = blueBounds[0]
+        self.b_upperBound = blueBounds[1]
+        self.cap = cam
+        self.count = 0
+        self.found_blue = 0
+        self.found_green = 0
+        self.found_red = 0
+        self.kernelOpen = np.ones((5, 5))
+        self.kernelClose = np.ones((20, 20))
+        self.b_area_list = []
+        self.g_area_list = []
+        self.r_area_list = []
+        self.font = cv2.FONT_HERSHEY_COMPLEX
+        self.px_cols = px_cols
+        self.px_rows = px_rows
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.out = cv2.VideoWriter("output.avi", self.fourcc, 10.0, (self.px_cols, self.px_rows))
+
+        self.ret, self.frame = self.cap.read()
+        # Resized to process faster
+        self.frame = cv2.resize(self.frame, (720, 540))
+        self.r_frame = cv2.resize(self.frame, (self.px_cols, self.px_rows))
+        # self.r_frame1 = cv2.resize(frame,(340,220))
+        self.r_frame = self.denoise(self.r_frame)
+        # denoise(self.r_frame1)
+        self.frame_HSV = cv2.cvtColor(self.r_frame, cv2.COLOR_BGR2HSV)
+
+    def checkArea(self, area, lower, upper):
+        if area > lower and area < upper:
+            return 1
+        else:
+            return 0
+
+    def denoise(self, frame):
+        frame = cv2.medianBlur(frame, 5)
+        frame = cv2.GaussianBlur(frame, (5, 5), 0)
+        return frame
+
+    def filter(self):
+        # Create filter for red colour
+        self.r_mask = cv2.inRange(self.frame_HSV, self.r_lowerBound, self.r_upperBound)
+        self.r_maskOpen = cv2.morphologyEx(self.r_mask, cv2.MORPH_OPEN, self.kernelOpen)
+        self.r_maskClose = cv2.morphologyEx(self.r_maskOpen, cv2.MORPH_CLOSE, self.kernelClose)
+        self.r_maskFinal = self.denoise(self.r_maskClose)
+
+        # Create filter for green colour
+        self.g_mask = cv2.inRange(self.frame_HSV, self.g_lowerBound, self.g_upperBound)
+        self.g_maskOpen = cv2.morphologyEx(self.g_mask, cv2.MORPH_OPEN, self.kernelOpen)
+        self.g_maskClose = cv2.morphologyEx(self.g_maskOpen, cv2.MORPH_CLOSE, self.kernelClose)
+        self.g_maskFinal = self.denoise(self.g_maskClose)
+        self.g_maskFinal = self.denoise(self.g_maskClose)
+
+        # Create filter for blue colour
+        self.b_mask = cv2.inRange(self.frame_HSV, self.b_lowerBound, self.b_upperBound)
+        self.b_maskOpen = cv2.morphologyEx(self.b_mask, cv2.MORPH_OPEN, self.kernelOpen)
+        self.b_maskClose = cv2.morphologyEx(self.b_maskOpen, cv2.MORPH_CLOSE, self.kernelClose)
+        self.b_maskFinal = self.denoise(self.b_maskClose)
+        self.b_maskFinal = self.denoise(self.b_maskFinal)
+
+    def makeContours(self):
+
+        # Find contours in the red & yellow masks
+        r_im, self.r_contours, r_hier = cv2.findContours(self.r_maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        g_im, self.g_contours, g_hier = cv2.findContours(self.g_maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        b_im, self.b_contours, b_hier = cv2.findContours(self.b_maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        self.contours = [self.b_contours, self.g_contours, self.r_contours]
+
+    def drawFrame(self):
+
+        # Iterate through contours for each colour & draw a box round a sufficient cluster.
+
+        for i in range(len(self.contours)):
+            if i == 0:
+                cv2.drawContours(self.r_frame, self.contours[i], -1, (255, 0, 0), 3)
+            elif i == 1:
+                cv2.drawContours(self.r_frame, self.contours[i], -1, (0, 255, 0), 3)
+            else:
+                cv2.drawContours(self.r_frame, self.contours[i], -1, (0, 0, 255), 3)
+            # cv2.drawContours(self.frame_HSV, contours[i], -1, (0, 255, 255), 3)
+            for j in range(len(self.contours[i])):
+                x, y, w, h = cv2.boundingRect(self.contours[i][j])
+                if i == 0:
+                    # print "Blue Height: %s\n Blue Width: %s" % (h,w)
+                    # The colour blue
+                    colour = (255, 0, 0)
+                    cv2.circle(self.r_frame, (x, y), 2, colour, 1)
+                    # The area of the blue object contour in rows * cols
+                    self.b_area = cv2.contourArea(self.contours[i][j])
+                    # Function checks area's within bounds
+
+                    if self.checkArea(self.b_area, 100, 500):
+                        # Draws a rectangle around the contour bounds
+                        cv2.rectangle(self.r_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                        print "Co-ords: ", (x, y)
+
+                    print "Blue Area: ", self.b_area
+                    if self.b_area > 40 and self.count > 3 and len(self.b_area_list) < 3:
+                        self.b_area_list.append(self.b_area)
+                    elif self.b_area > 40 and len(self.b_area_list) > 2:
+                        self.b_area_list.append(self.b_area)
+                        b_len = len(self.b_area_list)
+                        if (self.b_area_list[b_len - 2] + self.b_area_list[b_len - 1]) / 2 > 40:
+                            if self.checkArea(self.b_area, 40, 1000):
+                                # print "Blue Object Found"
+                                self.found_blue += 1
+
+                elif i == 1:
+
+                    self.g_area = cv2.contourArea(self.contours[i][j])
+                    # print "Green Height: %s\n Green Width: %s" % (h, w)
+                    colour = (0, 255, 0)
+                    cv2.circle(self.r_frame, (x, y), 2, colour, 2)
+                    if self.checkArea(self.g_area, 100, 500):
+                        cv2.rectangle(self.r_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    print "Green Area: ", self.g_area
+                    if self.g_area > 40 and self.count > 3 and len(self.g_area_list) < 3:
+                        self.g_area_list.append(self.g_area)
+                    elif self.g_area > 40 and len(self.g_area_list) > 2:
+                        self.g_area_list.append(self.g_area)
+                        g_len = len(self.g_area_list)
+                        if (self.g_area_list[g_len - 2] + self.g_area_list[g_len - 1]) / 2 > 40:
+                            # print "Green Object Found"
+                            self.found_green += 1
+                else:
+                    cv2.circle(self.r_frame, (x + w, y + h), 2, (0, 0, 255), 4)
+                    self.r_area = cv2.contourArea(self.contours[i][j])
+                    colour = (0, 0, 255)
+                    if self.checkArea(self.r_area, 100, 500):
+                        cv2.rectangle(self.r_frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                    print "Red Area:", self.r_area
+                    if self.r_area > 40 and self.count > 3 and len(self.r_area_list) < 3:
+                        self.r_area_list.append(self.r_area)
+
+                    elif self.r_area > 40 and len(self.r_area_list) > 2:
+                        self.r_area_list.append(self.r_area)
+                        r_len = len(self.r_area_list)
+                        if (self.r_area_list[r_len - 2] + self.r_area_list[r_len - 1]) / 2 > 40:
+                            if self.checkArea(self.r_area, 100, 1000):
+                                # print "Red Object Found"
+                                self.found_red += 1
+
+                # Write the contour rectangle index in subscript of the rectangle
+                cv2.putText(self.r_frame, "%s, %s" % (str(x), str(y)), (x - 20, y + h + 20
+                                                                   ), self.font, fontScale=0.5, color=colour, thickness=2)
+
+        # print len(x_list), len(y_list)
+        for px in range(self.px_cols / 40):
+            cv2.line(self.r_frame, ((px * 40), 0), ((px * 40), 7), thickness=1, color=(0, 0, 0))
+            cv2.putText(self.r_frame, str(px * 40), ((px * 40), 25), color=(0, 0, 0), thickness=1, fontScale=1,
+                        fontFace=cv2.FONT_HERSHEY_PLAIN)
+
+            # Show all the windows
+        cv2.imshow("Blue Mask", self.b_maskClose)
+        cv2.imshow("Red Mask", self.r_maskClose)
+        cv2.imshow("Green Mask", self.g_maskClose)
+        cv2.imshow("Contoured Frame", self.r_frame)
+        #if self.count > 20:
+            #self.out.write(self.r_frame)
+        self.count += 1
+        print "\n ----%s----- \n" % str(self.count)
